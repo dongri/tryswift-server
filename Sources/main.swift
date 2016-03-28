ce7c41b068c31e9225db39fcc25bf7fd3e7684f7
Dongri Jin <dongrify@gmail.com>
2016-03-28 22:47:29 +0900

    Added github repo

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,7 +4,9 @@ import Vapor
 let app = Application()
 
 app.get("/") { request in
-    return "Hello, 🌏, Swift!"
+  let hello_world = "Hello, 🌏, Swift!"
+  let github_url = "<p><a href='https://github.com/dongri/tryswift-server'>Github Repo</a></p>"
+  return hello_world + github_url
 }
 
 app.get("/json") { request in
